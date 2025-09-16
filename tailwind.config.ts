e6c3fbb0452553bc671d82a87d8fd29553e6835c
Ahmed Yahia Lashin <qarajeen@gmail.com>
2025-09-16 12:52:51 +0000

    in the hero section, there is glitch in the text

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -91,7 +91,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'fade-in-up': 'fade-in-up 0.5s ease-out forwards',
+        'fade-in-up': 'fade-in-up 0.5s ease-out',
       },
     },
   },
