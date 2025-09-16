d1953641a364e679e2f17c88dc59e697b7b38fb8
Ahmed Yahia Lashin <qarajeen@gmail.com>
2025-09-16 14:03:41 +0000

    make the layer of this bubble above "STUDIOO" title (_for element img_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ const spheres = [
   { id: 2, size: 250, top: '10%', left: '60%', animation: 'float-in-2', floatAnimation: 'float-2', duration: '10s', delay: '0.3s', z: 30, hint: 'data analytics' },
   { id: 3, size: 100, top: '25%', left: '40%', animation: 'float-in-5', floatAnimation: 'float-3', duration: '12s', delay: '0.5s', z: 5, hint: 'minimalist lamp' },
   { id: 4, size: 120, top: '50%', left: '5%', animation: 'float-in-3', floatAnimation: 'float-1', duration: '9s', delay: '0.2s', z: 25, hint: 'code snippet' },
-  { id: 5, size: 200, top: '60%', left: '30%', animation: 'float-in-6', floatAnimation: 'float-2', duration: '11s', delay: '0.4s', z: 5, hint: 'wireframe globe' },
+  { id: 5, size: 200, top: '60%', left: '30%', animation: 'float-in-6', floatAnimation: 'float-2', duration: '11s', delay: '0.4s', z: 25, hint: 'wireframe globe' },
   { id: 6, size: 180, top: '45%', left: '75%', animation: 'float-in-4', floatAnimation: 'float-3', duration: '13s', delay: '0.1s', z: 5, hint: 'blurry gradient' },
   { id: 7, size: 80, top: '75%', left: '85%', animation: 'float-in-2', floatAnimation: 'float-1', duration: '7s', delay: '0.6s', z: 25, hint: 'purple crystal' },
   { id: 8, size: 90, top: '80%', left: '15%', animation: 'float-in-3', floatAnimation: 'float-2', duration: '10s', delay: '0.7s', z: 5, hint: 'user portrait' },
