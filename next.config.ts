b03a538603726fc9121339390190c82c21347743
Ahmed Yahia Lashin <qarajeen@gmail.com>
2025-09-17 09:38:20 +0000

    when i publish the project to github, the images are not shown

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type {NextConfig} from 'next';
 
+const repoName = 'wrh-quote1';
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}`,
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -9,6 +13,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
