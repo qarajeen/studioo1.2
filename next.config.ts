ca647b70d7e566a4c217d81c895375b2355bf254
Ahmed Yahia Lashin <qarajeen@gmail.com>
2025-09-17 12:09:58 +0000

    the github repository name is studioo1

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from 'next';
 
-const repoName = process.env.NODE_ENV === 'production' ? '/wrh-quote1' : '';
+const repoName = process.env.NODE_ENV === 'production' ? '/studioo1' : '';
 
 const nextConfig: NextConfig = {
   output: 'export',
