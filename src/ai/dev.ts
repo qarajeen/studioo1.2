5ca7460b15b57c8d627406649e804f9c76cb1779
Ahmed Yahia Lashin <qarajeen@gmail.com>
2025-09-19 13:50:56 +0000

    Website Performance Analyzer tool, make it work without ai flow

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,9 +1,9 @@
 
 // Flows will be imported for their side effects in this file.
 // import './flows/summarize-quote-flow';
-import './flows/analyze-website-flow';
 import './flows/brand-color-checker-flow';
 import './flows/generate-event-checklist-flow';
+
 
 
 
