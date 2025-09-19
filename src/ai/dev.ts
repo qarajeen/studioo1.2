9911384e1461aa5ad0ddc98490f086d7e5b06967
Ahmed Yahia Lashin <qarajeen@gmail.com>
2025-09-19 11:24:54 +0000

    no. remove the ai feature

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -2,4 +2,3 @@
 // Flows will be imported for their side effects in this file.
 // import './flows/summarize-quote-flow';
 import './flows/analyze-website-flow';
-import './flows/analyze-image-quality-flow';
