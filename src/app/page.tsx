8a16c309ed2b7664e3af62a1f287c15c091ceea7
Ahmed Yahia Lashin <qarajeen@gmail.com>
2025-09-22 14:58:03 +0000

    nothing changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
         </div>
       </main>
 
-      <div className="absolute inset-0 z-10 w-full h-full flex items-end justify-center pointer-events-none pb-24">
+      <div className="absolute inset-0 z-10 w-full h-full flex items-end justify-center pointer-events-none mt-48 pb-0">
         <div className="w-full h-96 pointer-events-auto">
             <Carousel3D items={spheresData} />
         </div>
